7aa0193685902e3af3a1919d243bb582db25d127
vshorst <vincenthorst@hotmail.com>
2017-07-16 20:03:58 +0200

    first commit

--- spritz.py
+++ spritz.py
@@ -1,1 +1,11 @@
-print("hallo vincent")
+import numpy as np
+
+# Open text file
+results = []
+with open('spritz.txt') as inputfile:
+    for line in inputfile:
+        results.append(line.strip().split(' '))
+
+
+
+print (results)
