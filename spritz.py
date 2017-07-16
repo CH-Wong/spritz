d182e276c69854f7adf98dffcbfcb202cc81d2e2
Chun Heung Wong <w.chunheung@gmail.com>
2017-07-16 20:16:14 +0200

    poging 2

--- spritz.py
+++ spritz.py
@@ -6,7 +6,7 @@ with open('spritz.txt') as inputfile:
     for line in inputfile:
         results.append(line.strip().split(' '))
 
-print("HALLO DIT HEB IK NET VERANDERD OP VINCENTS PC!")
+print("HALLO DIT HEB IK NET VERANDERD OP VINCENTS dunne PC!")
 
 print("MERGE FOUT 2")
 
